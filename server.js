5400e0513d73289a97117c198a1bb7fc306ee010
RafaNM <72399004+RafaNM@users.noreply.github.com>
2021-04-27 08:05:21 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const exphbs = require('express-handlebars')
+const connectDB = require('./config/db')
+
+// Load config
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+const app = express()
+
+// Handlebars
+app.engine(
+  '.hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+)
+app.set('view engine', '.hbs')
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'static')))
+
+// Routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
