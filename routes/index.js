d1c7e39bb67a32e181d06188efced96d4a07bdff
RafaNM <72399004+RafaNM@users.noreply.github.com>
2021-04-27 08:06:23 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const hotel = require('../models/hotel');
+
+//@route get /
+router.get('/', async (req, res) => {
+    try{
+        const hotels = await hotels.find().sort( { prating: -1 } ).limit(5).lean();
+        res.render('home', {
+            hotels,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/404')
+    }
+})
+
+// @desc about
+// @route GET /about
+router.get('/about', (req, res) => {
+    res.render('about')
+})
+
+module.exports = router;
